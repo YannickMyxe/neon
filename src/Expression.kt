9f0710f21519dd0dfb55335ad6e8c4db64f213de
YannickMyxe <vankerckvoordeyannick@gmail.com>
2022-02-10 00:19:36 +0100

    added subclasses to the expressions

--- src/Expression.kt
+++ src/Expression.kt
@@ -1,31 +1,46 @@
-sealed class Expression{
+sealed class Expression {
 
-    class Value(val v: Int) : Expression()
+    sealed class Literal : Expression() {
+        class Value(val v: Int) : Literal()
+    }
 
-    class Add(val left: Expression, val right: Expression) : Expression()
+    sealed class Binary : Expression() {
+        class Add(val left: Expression, val right: Expression) : Binary()
+        class Subtract(val left: Expression, val right: Expression) : Binary()
+        class Multiply(val left: Expression, val right: Expression) : Binary()
+        class Divide(val left: Expression, val right: Expression) : Binary()
+    }
 
-    class Subtract(val left: Expression, val right: Expression) : Expression()
-
-    class Multiply(val left: Expression, val right: Expression): Expression()
-    class Divide(val left: Expression, val right: Expression): Expression()
+    sealed class Unary : Expression() {
+        class Negate(val left: Expression) : Unary()
+    }
 }
+
 
 fun evaluate(ex: Expression): Int {
     return when (ex) {
-        is Expression.Value -> ex.v
-        is Expression.Add -> evaluate(ex.left) + evaluate(ex.right)
-        is Expression.Subtract -> evaluate(ex.left) - evaluate(ex.right)
-        is Expression.Divide -> evaluate(ex.left) / evaluate(ex.right)
-        is Expression.Multiply -> evaluate(ex.left) * evaluate(ex.right)
+        // Literal
+        is Expression.Literal.Value -> ex.v
+        // Binary
+        is Expression.Binary.Add -> evaluate(ex.left) + evaluate(ex.right)
+        is Expression.Binary.Subtract -> evaluate(ex.left) - evaluate(ex.right)
+        is Expression.Binary.Divide -> evaluate(ex.left) / evaluate(ex.right)
+        is Expression.Binary.Multiply -> evaluate(ex.left) * evaluate(ex.right)
+        // Unary
+        is Expression.Unary.Negate -> -1 * evaluate(ex.left)
     }
 }
 
 fun evalToString(ex: Expression): String {
     return when (ex) {
-        is Expression.Value -> ex.v.toString()
-        is Expression.Add -> "(${evalToString(ex.left)} + ${evalToString(ex.right)})"
-        is Expression.Subtract -> "(${evalToString(ex.left)} - ${evalToString(ex.right)})"
-        is Expression.Divide -> "(${evalToString(ex.left)} / ${evalToString(ex.right)})"
-        is Expression.Multiply -> "(${evalToString(ex.left)} * ${evalToString(ex.right)})"
+        // Literal
+        is Expression.Literal.Value -> ex.v.toString()
+        // Binary
+        is Expression.Binary.Add -> "(${evalToString(ex.left)} + ${evalToString(ex.right)})"
+        is Expression.Binary.Subtract -> "(${evalToString(ex.left)} - ${evalToString(ex.right)})"
+        is Expression.Binary.Divide -> "(${evalToString(ex.left)} / ${evalToString(ex.right)})"
+        is Expression.Binary.Multiply -> "(${evalToString(ex.left)} * ${evalToString(ex.right)})"
+        // Unary
+        is Expression.Unary.Negate -> "(-${evalToString(ex.left)})"
     }
 }
