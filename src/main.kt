c041cabcc950e6591ff6b548890472da3cddb442
YannickMyxe <vankerckvoordeyannick@gmail.com>
2022-02-10 00:19:12 +0100

    Removed the tree, added timer

--- src/main.kt
+++ src/main.kt
@@ -1,18 +1,12 @@
-import Expression.*
 import tokens.Tokenizer
+import kotlin.system.measureTimeMillis
 
 fun main() {
     // The constructor will not initialize! Change the constructors to change this behavior
     val tokens = Tokenizer("comp.neon")
-    if(tokens.initialize()) println("Complete!")
-
-    val ex =
-    Add(
-        Multiply(Value(2), Value(3)),
-        Divide(Value(10), Value(2))
-    )
-
-    println("Expression: ${evalToString(ex)}")
-    println("Evaluation: ${evaluate(ex)}")
-
+    val tokenizerTime = measureTimeMillis {
+        if (tokens.initialize()) println("Complete!")
+        else println("Failed!")
+    }
+    println("[tokenizer] took [$tokenizerTime ms] to tokenize the file")
 }
